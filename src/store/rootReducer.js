ed7a0e3f1a9444cee7670d5d92f2743708450cb3
Matúš Holotňák <matus.holotnak@gmail.com>
2022-06-15 00:48:01 +0200

    implement reducers

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,1 +1,14 @@
-// TODO combineReducers export
+import { combineReducers } from 'redux';
+import authReducer from './auth/Auth.reducers';
+import cartReducer from './cart/Cart.reducers';
+import orderReducer from './orders/Orders.reducers';
+import productReducer from './products/Products.reducers';
+import userReducer from './user/User.reducers';
+
+export default combineReducers({
+  auth: authReducer,
+  user: userReducer,
+  products: productReducer,
+  cart: cartReducer,
+  order: orderReducer
+});
